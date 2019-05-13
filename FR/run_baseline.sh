31ce7573382cd68ca2da24bcc203d2ca995ee67c
arushi12130 <arushi.jain@mail.mcgill.ca>
2019-05-13 14:53:19 -0400

    Variance related risk Criteria baseline

---
+++ FR/run_baseline.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+Lr_p=(1e-3)
+Lr_c=(1e-1)
+Lam=(0.4)
+Temp=(5e-2)
+B=(1000 500 2500 1500 3000)
+Seed=(10)
+Episodes=(800)
+
+for temp in "${Temp[@]}"; do
+	for lr_p in "${Lr_p[@]}"; do
+		for lr_c in "${Lr_c[@]}"; do
+			for b in "${B[@]}"; do
+				for lam in "${Lam[@]}"; do
+				    for eps in "${Episodes[@]}"; do
+					    nohup python VarianceBaseline.py --temperature $temp  --lr_theta $lr_p --lr_critic $lr_c --b $b --lam $lam  --nepisodes $eps &
+					done
+				done
+			done
+		done
+	done
+done
